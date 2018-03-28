9956a1e109e7db0059a1979f6b7a08138df64832
Diego-UBUNTU <disalvatorediego@gmail.com>
2018-03-28 16:28:41 -0400

    Organizacion de la app

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -6,11 +6,11 @@
 		public function getIndex(){
 			//TEST
 			//To test the counter equals $actualDate to a date in AAAA-MM-DD in the line 8 and uncomment it. Comment line 12
-			//$actualDate = '2018-04-31';
+			$actualDate = '2018-04-31';
 			//
 			date_default_timezone_set('America/Caracas');
 
-			$actualDate = date('Y-m-d',time()); //Get actual date (string in AAAA-MM-DD)
+			//$actualDate = date('Y-m-d',time()); //Get actual date (string in AAAA-MM-DD)
 			$actualDate = date_create($actualDate); //Set actual date (type DATE)
 			$finalDate = date_create('2018-03-31'); //Set last day (type DATE)
 			$days = 0; //Days left
@@ -27,11 +27,11 @@
 			}else{
 				//COUNTER OFF
 				return $this->render('../views/login.php');
-			}
-			
+			}	
+		}
 
-
-			
+		public function postIndex(){
+			return $this->render('../views/main.php');
 		}
 	}
 	
