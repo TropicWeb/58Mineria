1570349f9f41b6bf9ea8c34abd104efd9ee93dd4
Miguel PC <amomymoder@gmail.com>
2018-03-28 20:21:24 -0400

    Ultimos Arreglos

--- views/main.php
+++ views/main.php
@@ -178,6 +178,7 @@
         </video>
       </div>
       <p class="text-center mar"><a href="logout" class="out">Salir</a></p>
+      <p class="text-center mar"><a href="logout" class="out">Reporte de entradas</a></p>
     </div>
   </div>
 </section>
